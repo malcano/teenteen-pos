df80db07db8024a403e1b79f1b75d486177b6649
마루우루 <marupedia@maluuluui-MacBookPro.local>
2025-03-03 01:10:04 +0900

    PWA Settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {VitePWA} from 'vite-plugin-pwa' // -> 이 친구
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Vite PWA Project',
+        short_name: 'Vite PWA Project',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'icon-48x48.png',
+                sizes: '48x48',
+                type: 'image/png'
+            },
+            {
+                src: 'icon-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+      }, 
+    })
+
   ],
 })
