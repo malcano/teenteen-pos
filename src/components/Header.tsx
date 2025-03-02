46977a55a390d7d5665146fa81551c22c49daf4c
마루우루 <marupedia@maluuluui-MacBookPro.local>
2025-03-03 01:32:18 +0900

    fix width of buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,3 @@
-// // src/components/Header.tsx
-// import React from "react";
-// import { useCart } from "../context/CartContext";
-
-// const Header = () => {
-//   const { total } = useCart();
-//   return (
-//     <div className="bg-gray-800 text-white text-2xl font-bold p-4 text-center">
-//       총 금액: {total.toLocaleString()}원
-//     </div>
-//   );
-// };
-
-// export default Header;
-
 // src/components/Header.tsx
 import { useCart } from "../context/CartContext";
 
